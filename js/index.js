aef137d7684ee87fb1bfe2907e3260121cc8846d
OlehKonyk <drmom4ik@gmail.com>
2024-01-31 18:47:24 +0200

    add buttons

--- js/index.js
+++ js/index.js
@@ -13,3 +13,14 @@ function changeEmailText() {
 function restoreEmailText() {
     document.getElementById("emailButton").innerHTML = "Send Email";
 }
+
+document.getElementById("phoneButton").addEventListener("click", makeCall);
+  document.getElementById("emailButton").addEventListener("click", sendEmail);
+  
+function makeCall() {
+    window.location.href = "tel:+380976275596";
+  }
+
+  function sendEmail() {
+    window.location.href = "mailto:drmom4ik@gmail.com";
+  }
